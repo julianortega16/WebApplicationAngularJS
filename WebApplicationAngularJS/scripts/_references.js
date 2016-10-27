a5a48e6f4c361a74491079401e37492729696f78
Julián Ortega <julian.ortega16@hotmail.com>
2016-10-26 22:14:50 -0500

    Creación Proyecto AngularJS

---
+++ WebApplicationAngularJS/scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../app.js" />
+/// <reference path="ai.0.22.19-build00125.js" />
+/// <reference path="angular.js" />
+/// <reference path="angular-mocks.js" />
